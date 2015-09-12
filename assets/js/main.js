f086aa152f7bd1559dcc7b5218dbd7a2d2864bce
dairdev <dennis.dair@gmail.com>
2015-09-12 14:18:47 -0500

    Change background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,21 @@
 //		//
 //	});
 //});
+$(function(){
+	var i=1;
+	var t=1;
+	function changeBck(){
+		if(t==6){
+			if(i==4){
+				i=1;
+			}
+			var imgUrl = 'assets/pictures/img' + i + '.jpg';
+			$("header").css('background-image', 'url(' + imgUrl + ')');
+			i++;
+			t=1;
+		}
+		t++;
+	}
+
+	setInterval(changeBck,1000);
+});
